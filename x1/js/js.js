ed8b490e3d6fa9eb190925f4e6b4825a8dddbaa3
kasanelat <kasane@coini.lat>
2025-03-22 13:43:34 -0700

    Update js.js

--- x1/js/js.js
+++ x1/js/js.js
@@ -5,10 +5,10 @@ function updateCountdown() {
     // Si no hay una fecha objetivo almacenada o si la fecha es inválida, establecer una nueva fecha objetivo
     if (!targetDate || isNaN(new Date(targetDate))) {
         targetDate = new Date(); // Fecha actual
-        targetDate.setDate(targetDate.getDate() + 0); // Sumar 4 días
-        targetDate.setHours(20); // Establecer las horas a 19:00
-        targetDate.setMinutes(33); // Establecer los minutos a 38
-        targetDate.setSeconds(57); // Establecer los segundos a 26
+        targetDate.setDate(targetDate.getDate() + 1); // Sumar 4 días
+        targetDate.setHours(15); // Establecer las horas a 19:00
+        targetDate.setMinutes(57); // Establecer los minutos a 38
+        targetDate.setSeconds(39); // Establecer los segundos a 26
         localStorage.setItem('targetDate', targetDate);
     } else {
         targetDate = new Date(targetDate); // Convertir la fecha almacenada de cadena a objeto de fecha
